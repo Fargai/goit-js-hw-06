f2bf3ee174c171a162bf9e1ae857190fe3a137e1
Natalka Pashchuk <fargai@ukr.net>
2023-03-20 17:44:48 +0200

    Add task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingsEl = document.querySelector("#ingredients");
+
+const elements = ingredients.map(ing => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = ing;
+  ingredient.classList.add("item");
+
+  return ingredient;
+})
+
+console.log(elements);
+
+ingsEl.append(...elements);
