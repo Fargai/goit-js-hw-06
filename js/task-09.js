cc01be3e23bf4d589eb7d5933c44620c56106743
Natalka Pashchuk <fargai@ukr.net>
2023-03-25 11:15:58 +0200

    Add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = document.body.style.backgroundColor;
+  // colorEl.textContent = getRandomHexColor();
+});
