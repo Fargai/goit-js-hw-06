c6045bfaa57308b3076b80fd248e69fe1bd75879
Natalka Pashchuk <fargai@ukr.net>
2023-03-25 10:23:55 +0200

    Add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+event.preventDefault();
+const {
+    elements: { email, password }
+} = event.currentTarget;
+
+if (email.value === "" || password.value === "") {
+    return alert("Please fill in all fields!");
+}
+const dataForm = {
+    email: email.value,
+    password: password.value,
+};
+console.log(dataForm);
+event.currentTarget.reset();
+};
